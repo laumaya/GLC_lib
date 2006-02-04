b2893b2f33e6203935e132e954fcf5fe6152cffa
laumaya <laumaya@3a52cc46-332c-0410-a947-8d4cafa376f5>
2006-02-04 11:48:30 +0000

    New mesh data structure. first commit

---
+++ glc_mesh2.cpp
@@ -0,0 +1,283 @@
+/****************************************************************************
+
+ This file is part of the GLC-lib library.
+ Copyright (C) 2005-2006 Laurent Ribon (laumaya@users.sourceforge.net)
+ Version 0.9.4, packaged on January, 2006.
+
+ http://glc-lib.sourceforge.net
+
+ GLC-lib is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLC-lib is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLC-lib; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*****************************************************************************/
+
+//! \file glc_mesh.cpp implementation of the GLC_Mesh2 class.
+
+#include <QtDebug>
+#include <assert.h>
+#include "glc_mesh2.h"
+
+//////////////////////////////////////////////////////////////////////
+// Constructor Destructor
+//////////////////////////////////////////////////////////////////////
+GLC_Mesh2::GLC_Mesh2(const GLfloat *pColor)
+:GLC_Geometry("Mesh", pColor)
+{
+
+}
+
+
+GLC_Mesh2::~GLC_Mesh2(void)
+{
+	m_CoordinateHash.clear();
+	m_CoordinateIndex.clear();
+	
+	m_NormalHash.clear();
+	m_NormalIndex.clear();
+	
+	m_TextCoordinateHash.clear();
+	m_TextureIndex.clear();
+	
+}
+/////////////////////////////////////////////////////////////////////
+// Set Functions
+//////////////////////////////////////////////////////////////////////
+
+
+//! Add material to mesh
+void GLC_Mesh2::AddMaterial(int Index, GLC_Material &Material)
+{
+	//! \todo GLC_Material should have copy constructor
+	MaterialHash::const_iterator iMaterial= m_MaterialHash.find(Index);
+	// Check if the key is already use
+	assert(iMaterial != m_MaterialHash.end());
+	
+	// Add the coordinate to coordinate hash table
+	m_MaterialHash[Index]= Material;
+
+}
+
+
+// Add a vertex to mesh
+void GLC_Mesh2::AddCoordinate(int Index, GLC_Vector3d Coordinate)
+{
+	Vector3dHash::const_iterator iVector= m_CoordinateHash.find(Index);
+	// Check if the key is already use
+	
+	assert(iVector == m_CoordinateHash.end());
+	
+	// Add the coordinate to coordinate hash table
+	m_CoordinateHash[Index]= Coordinate;
+	
+}
+
+// Add Normal
+void GLC_Mesh2::AddNormal(int Index, GLC_Vector3d Normal)
+{
+	Vector3dHash::const_iterator iVector= m_NormalHash.find(Index);
+	// Check if the key is already use
+	
+	assert(iVector == m_NormalHash.end());
+	
+	// Add the coordinate to coordinate hash table
+	m_NormalHash[Index]= Normal;
+	
+}
+
+// Add texture coordinate
+void GLC_Mesh2::AddTextureCoordinate(int Index, GLC_Vector2d TextureCoordinate)
+{
+	Vector2dHash::const_iterator iVector= m_TextCoordinateHash.find(Index);
+	
+	// Check if the key is already in use	
+	assert(iVector == m_TextCoordinateHash.end());
+	
+	// Add the coordinate to coordinate hash table
+	m_TextCoordinateHash[Index]= TextureCoordinate;
+	
+}
+
+
+//! Add a face without texture coordinate
+void GLC_Mesh2::AddFace(const QVector<int> &Material, const QVector<int> &Coordinate, const QVector<int> &Normal)
+{
+	
+	AddMaterialIndex(Material);
+	AddCoordAndNormIndex(Coordinate, Normal);
+	
+	// Check if indexed list size
+	assert((m_NormalIndex.size() == m_CoordinateIndex.size()) && m_TextureIndex.isEmpty());
+	
+	// Increment number of faces
+	m_NumberOfFaces++;
+	
+}
+
+// Add a face with texture coordinate
+void GLC_Mesh2::AddFace(const QVector<int> &Material, const QVector<int> &Coordinate, const QVector<int> &Normal,
+						const QVector<int> &TextureCoordinate)
+{
+	AddMaterialIndex(Material);
+	AddCoordAndNormIndex(Coordinate, Normal);
+	AddTextureIndex(TextureCoordinate);
+	
+	// Check if indexed list have the same size
+	assert(m_TextureIndex.size() == m_NormalIndex.size());
+	assert(m_TextureIndex.size() == m_CoordinateIndex.size());
+	
+	// Increment number of faces
+	m_NumberOfFaces++;
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// OpenGL Functions
+//////////////////////////////////////////////////////////////////////
+
+// Virtual interface for OpenGL Geometry set up.
+void GLC_Mesh2::GlDraw()
+{
+	// If the mesh is empty there is noting to do
+	if (m_CoordinateIndex.isEmpty()) return;
+	
+	const bool MeshIsTextured= !m_TextureIndex.isEmpty();
+	
+	// Index face separator
+	const int separator= -1;
+	
+	int CurrentMaterialIndex= m_MaterialIndex.at(0);
+	
+	bool IsNewFace= true;
+	
+	for(int i=0; i < m_CoordinateIndex.size(); ++i)
+	{
+		if (m_CoordinateIndex.at(i) == separator)
+		{			
+			assert((i != 0) && (!IsNewFace));	// the first index couldn't be a separator
+			// End of current face
+			glEnd();
+			// At the next round a new face will be create
+			IsNewFace=true;
+		}
+		else
+		{
+			if (IsNewFace)
+			{
+				if (CurrentMaterialIndex != m_MaterialIndex.at(i))
+				{	// If the material change, make it current
+					CurrentMaterialIndex= m_MaterialIndex.at(i);
+					m_MaterialHash[CurrentMaterialIndex].GlExecute();
+				}
+				IsNewFace= false;
+				// New polygon creation
+				glBegin(GL_POLYGON);
+			}
+				// Vertex texture coordinate if necessary
+				if (MeshIsTextured)
+					glTexCoord2dv(m_TextCoordinateHash[m_TextureIndex.at(i)].Return_dVect());
+				// Vertex Normal
+				glNormal3dv(m_NormalHash[m_NormalIndex.at(i)].Return_dVect());
+				// Vertex 3D coordinate
+				glVertex3dv(m_CoordinateHash[m_CoordinateIndex.at(i)].Return_dVect());									
+		}		
+	}
+}
+
+// Virtual interface for OpenGL Geomtry properties.
+void GLC_Mesh2::GlPropGeom()
+{
+		//! Change the current matrix
+		glMultMatrixd(m_MatPos.Return_dMat());
+		
+		if (m_IsBlended)
+		{
+			glEnable(GL_BLEND);
+			glDepthMask(GL_FALSE);
+		}
+		else
+		{
+			glDisable(GL_BLEND);
+		}		
+		
+		if(!m_pMaterial || (m_PolyMode != GL_FILL))
+		{
+			glDisable(GL_TEXTURE_2D);
+			glDisable(GL_LIGHTING);
+			glColor4fv(GetfRGBA());			// is color
+		}
+		else if (m_pMaterial->GetAddRgbaTexture())
+		{
+			glEnable(GL_TEXTURE_2D);
+			glEnable(GL_LIGHTING);
+			m_pMaterial->GlExecute();
+		}
+		else
+		{
+			glDisable(GL_TEXTURE_2D);
+			glEnable(GL_LIGHTING);
+			m_pMaterial->GlExecute();
+		}
+		
+		glLineWidth(GetThickness());	// is thikness
+
+		// Polygons display mode
+		glPolygonMode(m_PolyFace, m_PolyMode);
+
+}
+
+// Add coordinate and normal index of a face
+void GLC_Mesh2::AddCoordAndNormIndex(const QVector<int> &Coordinate, const QVector<int> &Normal)
+{	
+	// Number of coordinate to add
+	const int size= Coordinate.size();
+	
+	// Add Coordinate index of the face
+	for (int i= 0; i < size; ++i)
+	{
+		m_CoordinateIndex.append(Coordinate[i]);		
+	}
+	m_CoordinateIndex.append(-1); // End of the face's coordinate index
+
+	// Add Normal index of the face
+	for (int i= 0; i < size; ++i)
+	{
+		m_NormalIndex.append(Normal[i]);		
+	}
+	m_NormalIndex.append(-1); // End of the face's normal index
+	
+	// Check if indexed list have the same size
+	assert(m_NormalIndex.size() == m_CoordinateIndex.size());
+	
+}
+
+// Add Texture coordinate index of a face
+void GLC_Mesh2::AddTextureIndex(const QVector<int> &TextureCoordinate)
+{
+	for (int i= 0; i < TextureCoordinate.size(); ++i)
+	{
+		m_TextureIndex.append(TextureCoordinate[i]);		
+	}
+	m_TextureIndex.append(-1); // End of the face's texture coordinate index
+	
+}
+
+// Add Material index of a face
+void GLC_Mesh2::AddMaterialIndex(const QVector<int> &Material)
+{
+	for (int i= 0; i < Material.size(); ++i)
+	{
+		m_MaterialIndex.append(Material[i]);		
+	}
+	m_MaterialIndex.append(-1); // End of the face's material index
+}
---
+++ glc_mesh2.h
@@ -0,0 +1,161 @@
+/****************************************************************************
+
+ This file is part of the GLC-lib library.
+ Copyright (C) 2005-2006 Laurent Ribon (laumaya@users.sourceforge.net)
+ Version 0.9.4, packaged on January, 2006.
+
+ http://glc-lib.sourceforge.net
+
+ GLC-lib is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLC-lib is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLC-lib; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*****************************************************************************/
+
+//! \file glc_mesh.h interface for the GLC_Mesh2 class.
+
+#ifndef GLC_MESH2_H_
+#define GLC_MESH2_H_
+#include <QHash>
+#include <QList>
+#include <QVector>
+
+#include "glc_material.h"
+#include "glc_geometry.h"
+
+//! The mesh's faces list
+typedef QHash<int, GLC_Vector3d> Vector3dHash;
+typedef QHash<int, GLC_Vector2d> Vector2dHash;
+
+typedef QHash<int, GLC_Material> MaterialHash;
+
+typedef QList<int> IndexedList;
+
+//////////////////////////////////////////////////////////////////////
+//! \class GLC_Mesh2
+/*! \brief GLC_Mesh2 : OpenGL 3D Mesh*/
+
+/*! An GLC_Mesh2 is Mesh composed of polygons
+ * 		- Each polygon must be a planar Face
+ * 		- Each face is composed by vertexs
+ * */
+//////////////////////////////////////////////////////////////////////
+class GLC_Mesh2 : public GLC_Geometry
+{
+//////////////////////////////////////////////////////////////////////
+/*! @name Constructor / Destructor */
+//@{
+//////////////////////////////////////////////////////////////////////
+public:
+	//! Construct an empty Mesh
+	explicit GLC_Mesh2(const GLfloat *pColor= 0);
+	
+	//! Delete Mesh's faces and clear faces list
+	virtual ~GLC_Mesh2(void);
+//@}
+
+//////////////////////////////////////////////////////////////////////
+/*! \name Get Functions*/
+//@{
+//////////////////////////////////////////////////////////////////////
+public:
+
+//@}
+
+//////////////////////////////////////////////////////////////////////
+/*! \name Set Functions*/
+//@{
+//////////////////////////////////////////////////////////////////////
+public:
+	//! Add material to mesh
+	void AddMaterial(int Index, GLC_Material &Material);
+	
+	//! Add a vertex to mesh
+	void AddCoordinate(int Index, GLC_Vector3d Coordinate);
+	
+	//! Add Normal
+	void AddNormal(int index, GLC_Vector3d Normal);
+	
+	//! Add texture coordinate
+	void AddTextureCoordinate(int Index, GLC_Vector2d TextureCoordinate);
+		 
+	//! Add a face without texture coordinate
+	/*! Mesh list of texture index must be empty.
+	 */  
+	void AddFace(const QVector<int> &Material, const QVector<int> &Coordinate, const QVector<int> &Normal);
+
+	//! Add a face with texture coordinate
+	/*! Number of coordinate, Normal and texture must be equal
+	 */
+	void AddFace(const QVector<int> &Material, const QVector<int> &Coordinate, const QVector<int> &Normal,
+				const QVector<int> &TextureCoordinate);
+//@}
+
+//////////////////////////////////////////////////////////////////////
+/*! \name OpenGL Functions*/
+//@{
+//////////////////////////////////////////////////////////////////////
+private:
+	//! Virtual interface for OpenGL Geometry set up.
+	/*! This Virtual function is implemented here.\n*/
+	virtual void GlDraw(void);
+
+	//! Virtual interface for OpenGL Geomtry properties.
+	/*! This Virtual function is implemented here.*/
+	virtual void GlPropGeom(void);
+//@}
+
+//////////////////////////////////////////////////////////////////////
+//! Private services Functions
+//////////////////////////////////////////////////////////////////////
+private:
+
+	//! Add coordinate and normal index of a face
+	void AddCoordAndNormIndex(const QVector<int> &Coordinate, const QVector<int> &Normal);
+	
+	//! Add Texture coordinate index of a face
+	void AddTextureIndex(const QVector<int> &TextureCoordinate);
+	
+	//! Add Material index of a face
+	void AddMaterialIndex(const QVector<int> &Material);
+	
+//////////////////////////////////////////////////////////////////////
+// Private members
+//////////////////////////////////////////////////////////////////////
+private:
+
+		
+	//! Coordinate hash table
+	Vector3dHash m_CoordinateHash;
+	//! Coordinate index
+	IndexedList m_CoordinateIndex;
+	
+	//! Material Hash table
+	MaterialHash m_MaterialHash;
+	//! Material index
+	IndexedList m_MaterialIndex;
+	
+	//! Normal hash table
+	Vector3dHash m_NormalHash;
+	//! Normal Index
+	IndexedList m_NormalIndex;
+	
+	//! Texture coordinate Hash table
+	Vector2dHash m_TextCoordinateHash;
+	//! Texture index
+	IndexedList m_TextureIndex;
+	
+	int m_NumberOfFaces;
+	
+};
+#endif //GLC_MESH2_H_
