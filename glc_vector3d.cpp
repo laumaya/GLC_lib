8e53960b7561c76bec7b743e15b27c80fbdfcd58
laumaya <laumaya@3a52cc46-332c-0410-a947-8d4cafa376f5>
2006-02-04 11:50:42 +0000

    Add GLC_vector3d class to GLC_lib. First commit.

---
+++ glc_vector3d.cpp
@@ -0,0 +1,102 @@
+/****************************************************************************
+
+ This file is part of the GLC-lib library.
+ Copyright (C) 2005-2006 Laurent Ribon (laumaya@users.sourceforge.net)
+ Version 0.9.4, packaged on January, 2006.
+
+ http://glc-lib.sourceforge.net
+
+ GLC-lib is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLC-lib is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLC-lib; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*****************************************************************************/
+
+
+//! \file glc_vector3d.cpp implementation of the GLC_Vector3d class.
+#include <QtDebug>
+#include "glc_vector3d.h"
+
+
+//////////////////////////////////////////////////////////////////////
+// Constructor Destructor
+//////////////////////////////////////////////////////////////////////
+
+/*! Default constructor
+*  Value is set to 
+* \n X = 0.0
+* \n Y =  0.0
+* \n Z =  0.0
+*/
+
+GLC_Vector3d::GLC_Vector3d()
+{
+	dVecteur[0]= 0.0;
+	dVecteur[1]= 0.0;
+	dVecteur[2]= 0.0;
+
+}
+/*! Recopy constructor
+ * Sample use
+ * \code
+ * NewVect = new GLC_Vector3d(OldVect);
+ * \endcode
+ */
+GLC_Vector3d::GLC_Vector3d(const GLC_Vector3d &Vect)
+{
+	dVecteur[0]= Vect.dVecteur[0];
+	dVecteur[1]= Vect.dVecteur[1];
+	dVecteur[2]= Vect.dVecteur[2];
+
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// Fonction Set
+//////////////////////////////////////////////////////////////////////
+GLC_Vector3d& GLC_Vector3d::SetX(const double &dX)
+{
+	dVecteur[0]= dX;
+	return *this;
+}
+
+GLC_Vector3d& GLC_Vector3d::SetY(const double &dY)
+{
+	dVecteur[1]= dY;
+	return *this;
+}
+
+GLC_Vector3d& GLC_Vector3d::SetZ(const double &dZ)
+{
+	dVecteur[2]= dZ;
+	return *this;
+}
+
+
+GLC_Vector3d& GLC_Vector3d::SetVect(const double &dX, const double &dY, const double &dZ)
+{
+	dVecteur[0]= dX;
+	dVecteur[1]= dY;
+	dVecteur[2]= dZ;
+
+	return *this;
+}
+GLC_Vector3d& GLC_Vector3d::SetVect(const GLC_Vector3d &Vect)
+{
+	dVecteur[0]= Vect.dVecteur[0];
+	dVecteur[1]= Vect.dVecteur[1];
+	dVecteur[2]= Vect.dVecteur[2];
+	return *this;
+}
+
+
---
+++ glc_vector3d.h
@@ -0,0 +1,142 @@
+/****************************************************************************
+
+ This file is part of the GLC-lib library.
+ Copyright (C) 2005-2006 Laurent Ribon (laumaya@users.sourceforge.net)
+ Version 0.9.4, packaged on January, 2006.
+
+ http://glc-lib.sourceforge.net
+
+ GLC-lib is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLC-lib is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLC-lib; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*****************************************************************************/
+
+//! \file glc_vector3d.h interface for the GLC_Vector3d class.
+
+#ifndef GLC_VECTOR3D_H_
+#define GLC_VECTOR3D_H_
+
+#include "glc_utils_maths.h"
+//////////////////////////////////////////////////////////////////////
+// définition global
+//////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////
+//! \class GLC_Vector3d
+/*! \brief GLC_Vector3d is a 3 dimensions Vector*/
+
+/*! GLC_Vector3d is used to represent 3D position and vectors.
+ * */
+//////////////////////////////////////////////////////////////////////
+
+class GLC_Vector3d  
+{
+	
+//////////////////////////////////////////////////////////////////////
+/*! @name Constructor / Destructor */
+//@{
+//////////////////////////////////////////////////////////////////////
+public:
+	GLC_Vector3d();
+	/*! Standard constructor With x, y, z and w with default value of 1.0*/
+	GLC_Vector3d(const double &dX, const double &dY, const double &dZ)
+	{
+		SetVect(dX, dY, dZ);
+	}
+	// Recopy constructor
+	GLC_Vector3d(const GLC_Vector3d &Vect);
+//@}
+
+	
+//////////////////////////////////////////////////////////////////////
+/*! \name Set Functions*/
+//@{
+//////////////////////////////////////////////////////////////////////
+public:
+	/*! X Composante*/
+	GLC_Vector3d& SetX(const double &dX);
+	
+	/*! Y Composante*/
+	GLC_Vector3d& SetY(const double &dY);
+	
+	/*! Z Composante*/
+	GLC_Vector3d& SetZ(const double &dZ);
+	
+	/*! All Composante*/
+	GLC_Vector3d& SetVect(const double &dX, const double &dY,
+		const double &dZ);
+		
+	/*! From another Vector*/
+	GLC_Vector3d& SetVect(const GLC_Vector3d &Vect);
+	
+//@}
+
+//////////////////////////////////////////////////////////////////////
+/*! \name Get Functions*/
+//@{
+//////////////////////////////////////////////////////////////////////
+public:
+	/*! X Composante*/
+	inline const double GetX(void) const
+	{
+		return dVecteur[0];
+	}
+	/*! Y Composante*/
+	inline const double GetY(void) const
+	{
+		return dVecteur[1];
+	}
+	/*! Z Composante*/
+	inline const double GetZ(void) const
+	{
+		return dVecteur[2];
+	}
+	/*! retourne un pointeur constant vers le tableau du vecteur.*/
+	inline const double *Return_dVect(void) const
+	{
+		return dVecteur;
+	}
+	/*! Vector is null*/
+	inline const bool IsNull(void) const
+	{
+		bool result;
+
+		result= (fabs(dVecteur[0]) < EPSILON) && (fabs(dVecteur[1]) < EPSILON)
+			&& (fabs(dVecteur[2]) < EPSILON);
+		
+		return result;
+	}
+
+//@}
+
+//////////////////////////////////////////////////////////////////////
+// Private services functions
+//////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////
+//name Private attributes
+//////////////////////////////////////////////////////////////////////
+private:
+	/*! Vector array definition \n
+	*	dVecteur[0]	X \n
+	*	dVecteur[1]	Y \n
+	*	dVecteur[2]	Z \n
+	*/
+	double dVecteur[3];
+
+}; //class GLC_Vector3d
+
+
+#endif /*GLC_VECTOR3D_H_*/
