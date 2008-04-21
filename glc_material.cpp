2676e51b4c421a6bccff2648ce022711048ba854
laumaya <laumaya@3a52cc46-332c-0410-a947-8d4cafa376f5>
2008-04-21 18:04:37 +0000

    Fix a bug when render a mesh in whireframe, the renderer color is now diffuse.

--- glc_material.cpp
+++ glc_material.cpp
@@ -372,7 +372,7 @@ void GLC_Material::glExecute(GLenum Mode)
 		glBlendFunc(GL_SRC_ALPHA,GL_ONE);
 		m_pTexture->glcBindTexture();
 	}
-	glColor4fv(pAmbientColor);
+	glColor4fv(pDiffuseColor);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, pAmbientColor);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, pDiffuseColor);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, pSpecularColor);
