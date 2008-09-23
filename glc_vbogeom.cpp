680520888b6ac311c30c76620bdeea944942db36
laumaya <laumaya@3a52cc46-332c-0410-a947-8d4cafa376f5>
2008-09-23 19:43:26 +0000

    Add VboGeom abstract class. This class will replace GLC_Geometry class.

---
+++ glc_vbogeom.cpp
@@ -0,0 +1,246 @@
+/****************************************************************************
+
+ This file is part of the GLC-lib library.
+ Copyright (C) 2005-2008 Laurent Ribon (laumaya@users.sourceforge.net)
+ Version 1.0.0, packaged on August, 2008.
+
+ http://glc-lib.sourceforge.net
+
+ GLC-lib is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLC-lib is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLC-lib; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*****************************************************************************/
+
+//! \file glc_vbogeom.cpp Implementation of the GLC_VboGeom class.
+
+#include "glc_vbogeom.h"
+#include "glc_openglexception.h"
+#include "glc_selectionmaterial.h"
+
+//////////////////////////////////////////////////////////////////////
+// Constructor destructor
+//////////////////////////////////////////////////////////////////////
+// Default constructor
+GLC_VboGeom::GLC_VboGeom(const QString& name, const bool typeIsWire)
+:GLC_Object(name)
+, m_VertexBufferID(0)
+, m_NormalBufferID(0)
+, m_IndexBufferID(0)
+, m_ColorBufferID(0)
+, m_GeometryIsValid(false)	// By default geometry is invalid
+, m_pMaterial(NULL)			// have to be set later in constructor
+, m_UseColorPerVertex(false)
+, m_IsWire(typeIsWire)		// the geometry type
+, m_IsTransparent(false)	// Not transparent by default
+{	
+	// Material is set here
+	setMaterial(new GLC_Material());
+}
+// Copy constructor
+GLC_VboGeom::GLC_VboGeom(const GLC_VboGeom& sourceGeom)
+:GLC_Object(sourceGeom)
+, m_VertexBufferID(0)
+, m_NormalBufferID(0)
+, m_IndexBufferID(0)
+, m_ColorBufferID(0)
+, m_GeometryIsValid(false)	// By default geometry is invalid
+, m_pMaterial(NULL)			// have to be set later in constructor
+, m_UseColorPerVertex(sourceGeom.m_UseColorPerVertex)
+, m_IsWire(sourceGeom.m_IsWire)
+, m_IsTransparent(sourceGeom.m_IsTransparent)
+{
+	m_Uid= glc::GLC_GenID();
+	// Material is copy here
+	setMaterial(new GLC_Material());
+	m_pMaterial->setMaterial(sourceGeom.getMaterial());
+}
+
+GLC_VboGeom::~GLC_VboGeom()
+{
+	// Vertex VBO
+	if (0 != m_VertexBufferID)
+		glDeleteBuffers(1, &m_VertexBufferID);
+	// Normal VBO
+	if (0 != m_NormalBufferID)
+		glDeleteBuffers(1, &m_NormalBufferID);
+	// Index VBO
+	if (0 != m_IndexBufferID)
+		glDeleteBuffers(1, &m_IndexBufferID);
+	// Color VBO
+	if (0 != m_ColorBufferID)
+		glDeleteBuffers(1, &m_ColorBufferID);
+	// Material	
+	if (NULL != m_pMaterial)
+	{
+		m_pMaterial->delGLC_Geom(getID());	//Remove Geometry from the material usage collection
+		if (m_pMaterial->isUnused()) delete m_pMaterial;
+	}
+}
+
+/////////////////////////////////////////////////////////////////////
+// Get Functions
+//////////////////////////////////////////////////////////////////////
+
+// return the geometry bounding box
+GLC_BoundingBox* GLC_VboGeom::getBoundingBox(void) const
+{
+	return NULL;	
+}
+
+
+/////////////////////////////////////////////////////////////////////
+// Set Functions
+//////////////////////////////////////////////////////////////////////
+
+// Material
+void GLC_VboGeom::setMaterial(GLC_Material* pMat)
+{
+	if (pMat != m_pMaterial)
+	{
+		if (pMat != NULL)
+		{
+			pMat->addGLC_Geom(this);
+		}
+	
+		if (m_pMaterial != NULL)
+		{
+			m_pMaterial->delGLC_Geom(getID());
+			if (m_pMaterial->isUnused()) delete m_pMaterial;
+		}
+
+		m_pMaterial= pMat;
+		
+		// Test if the material is transparent
+		if (m_pMaterial->isTransparent())
+		{
+			setTransparency(true);
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////
+// OpenGL Functions
+//////////////////////////////////////////////////////////////////////
+
+// if the geometry have a texture, load it
+void GLC_VboGeom::glLoadTexture(void)
+{
+	m_pMaterial->glLoadTexture();
+}
+
+// Geometry display
+void GLC_VboGeom::glExecute(bool isSelected, bool forceWire)
+{
+	// Define Geometry's property
+	glPropGeom(isSelected, forceWire);
+
+	if (0 == m_VertexBufferID)
+	{
+		createVBOs();
+	}
+	else
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, m_VertexBufferID);
+		if (!m_IsWire)
+		{
+			glBindBuffer(GL_ARRAY_BUFFER, m_NormalBufferID);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBufferID);			
+		}
+		if (m_UseColorPerVertex)
+			glBindBuffer(GL_ARRAY_BUFFER, m_ColorBufferID);	
+	}
+	
+	glDraw();
+	m_GeometryIsValid= true;
+	
+	// Unbind VBOs
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	if (!m_IsWire)
+	{
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+
+	// OpenGL error handler
+	GLenum error= glGetError();	
+	if (error != GL_NO_ERROR)
+	{
+		GLC_OpenGlException OpenGlException("GLC_VboGeom::GlExecute ", error);
+		throw(OpenGlException);
+	}
+}
+
+// Virtual interface for OpenGL Geometry properties.
+void GLC_VboGeom::glPropGeom(bool isSelected, bool forceWire)
+{
+	
+	if(m_IsWire || forceWire)
+	{
+		glDisable(GL_TEXTURE_2D);
+		glDisable(GL_LIGHTING);
+		
+		if (isSelected) GLC_SelectionMaterial::glExecute();
+		else glColor4f(getdRed(), getdGreen(), getdBlue(), getdAlpha());			// is color
+	}
+	else if (m_pMaterial->getAddRgbaTexture())
+	{
+		glEnable(GL_TEXTURE_2D);
+		glEnable(GL_LIGHTING);
+		m_pMaterial->glExecute();
+		if (isSelected) GLC_SelectionMaterial::glExecute();		
+	}
+	else
+	{
+		glDisable(GL_TEXTURE_2D);
+		glEnable(GL_LIGHTING);
+		if (isSelected) GLC_SelectionMaterial::glExecute();
+		else m_pMaterial->glExecute();
+	}
+		
+	// OpenGL error handler
+	GLenum error= glGetError();	
+	if (error != GL_NO_ERROR)
+	{
+		GLC_OpenGlException OpenGlException("GLC_VboGeom::GlPropGeom ", error);
+		throw(OpenGlException);
+	}	
+}
+
+// Vbo creation
+void GLC_VboGeom::createVBOs()
+{
+	// Vertexs
+	if (0 == m_VertexBufferID)
+	{
+		glGenBuffers(1, &m_VertexBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, m_VertexBufferID);
+		// Normals
+		if ((0 == m_NormalBufferID) and !m_IsWire)
+		{
+			glGenBuffers(1, &m_NormalBufferID);
+			glBindBuffer(GL_ARRAY_BUFFER, m_NormalBufferID);
+		}
+		// Index
+		if ((0 == m_IndexBufferID) and !m_IsWire)
+		{
+			glGenBuffers(1, &m_IndexBufferID);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBufferID);
+		}
+	}
+	// Color
+	if ((0 == m_ColorBufferID) and m_UseColorPerVertex)
+	{
+		glGenBuffers(1, &m_ColorBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, m_ColorBufferID);
+	}
+}
---
+++ glc_vbogeom.h
@@ -0,0 +1,237 @@
+/****************************************************************************
+
+ This file is part of the GLC-lib library.
+ Copyright (C) 2005-2008 Laurent Ribon (laumaya@users.sourceforge.net)
+ Version 1.0.0, packaged on August, 2008.
+
+ http://glc-lib.sourceforge.net
+
+ GLC-lib is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLC-lib is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLC-lib; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*****************************************************************************/
+
+//! \file glc_vbogeom.h Interface for the GLC_VboGeom class.
+
+#ifndef GLC_VBOGEOM_H_
+#define GLC_VBOGEOM_H_
+#include <QColor>
+#include "glc_object.h"
+#include "glc_material.h"
+#include "glc_boundingbox.h"
+
+//////////////////////////////////////////////////////////////////////
+//! \class GLC_VboGeom
+/*! \brief GLC_VboGeom : parent class for all GLC class which contain
+ *  vbo geometrical data*/
+
+/*! GLC_VboGeom is a abstract class. \n \n
+ *  Main attributes of GLC_VboGeom:
+ *		- Material : 	GLC_Material
+ * 		- Graphic properties
+ * 		- Transformation Matrix
+ * 
+ * GLC_Geometry provide :
+ * 		- Function to create VBO : GLC_VboGeom::createVbo
+ * 		- Function to draw Geometry : GLC_VboGeom::glExecute
+ * 		- Virtual function to overload for visual property: GLC_VboGeom::glPropGeom
+ * 		- Virtual function to overload for Object topology: GLC_VboGeom::glDraw
+ *
+ */
+//////////////////////////////////////////////////////////////////////
+
+struct GLC_Vertex
+{
+	// Vertex
+	float x, y, z;			// 12 Bytes
+	// Normal
+	float nx, ny, nz;		// 12 Bytes
+	// Texture
+	float s, t;				// 8 Bytes
+	// => 32 Bytes
+};
+class GLC_VboGeom : public GLC_Object
+{
+//////////////////////////////////////////////////////////////////////
+/*! @name Constructor / Destructor */
+//@{
+//////////////////////////////////////////////////////////////////////
+public:
+	//! Default constructor
+	/*!
+	 * QString Name
+	 * const bool typeIsWire
+	 */
+	GLC_VboGeom(const QString &, const bool);
+	//! Copy constructor
+	/*!
+	 * const GLC_VboGeom geometry to copy
+	 */
+	GLC_VboGeom(const GLC_VboGeom&);
+	//! Destructor
+	virtual ~GLC_VboGeom();
+//@}
+
+//////////////////////////////////////////////////////////////////////
+/*! \name Get Functions*/
+//@{
+//////////////////////////////////////////////////////////////////////
+public:
+	//! Return a QColor which represent the color
+	inline QColor getRGBA(void) const
+	{return m_pMaterial->getDiffuseColor();}
+
+	//! Return Color Red component
+	inline GLfloat getdRed(void) const
+	{return m_pMaterial->getDiffuseColor().redF();}
+
+	//! Return Color Green component
+	inline GLfloat getdGreen(void) const
+	{return m_pMaterial->getDiffuseColor().greenF();}
+	
+	//! Return Color blue component
+	inline GLfloat getdBlue(void) const
+	{return m_pMaterial->getDiffuseColor().blueF();}
+	
+	//! Return Color Alpha component
+	inline GLfloat getdAlpha(void) const
+	{return m_pMaterial->getDiffuseColor().alphaF();}
+			
+	//! Return Validity of geometry
+	inline bool getValidity(void) const
+	{return m_GeometryIsValid;}
+
+	//! Return material of geometry
+	inline GLC_Material* getMaterial(void) const
+	{return m_pMaterial;}
+	
+	//! return the geometry bounding box
+	virtual GLC_BoundingBox* getBoundingBox(void) const;
+	
+	//! clone the geometry
+	virtual GLC_VboGeom* clone() const = 0;
+	
+	//! Get the geometry transparency
+	inline bool isTransparent() const
+	{return m_IsTransparent;}
+	
+	//! return true if color per vertex is used
+	inline bool usedColorPerVertex() const
+	{return m_UseColorPerVertex;}
+
+//@}
+	
+//////////////////////////////////////////////////////////////////////
+/*! \name Set Functions*/
+//@{
+//////////////////////////////////////////////////////////////////////
+public:
+
+	//! Set Diffuse Color RGBA component with an QColor Object
+	inline void setColor(const QColor& setCol)
+	{
+		m_pMaterial->setDiffuseColor(setCol);
+	}
+
+	// Material
+	//! Set the Geometry material
+	void setMaterial(GLC_Material* pMat);
+	
+	//! Set the transprency property
+	inline void setTransparency(const bool transparency)
+	{m_IsTransparent= transparency;}
+	
+	inline void colorPerVertex(const bool colorPerVertex)
+	{
+		if (m_UseColorPerVertex != colorPerVertex)
+		{
+			m_UseColorPerVertex= colorPerVertex;
+			m_GeometryIsValid= false;
+		}
+	}
+//@}
+//////////////////////////////////////////////////////////////////////
+/*! \name OpenGL Functions*/
+//@{
+//////////////////////////////////////////////////////////////////////
+public:
+	//! if the geometry have a texture, load it
+	virtual void glLoadTexture(void);
+	
+	//! Virtual interface for OpenGL execution from GLC_Object.
+	/*! This Virtual function is implemented here.\n
+	 * At the first call, this function call virtual function
+	 * GLC_VboGeom::glPropGeom and set up :
+	 * 		- Geometry
+	 * 		- VBO
+	 * AfterWard this function
+	 *		- Call virtual function GLC_VboGeom::glPropGeom
+	 *        virtual function GLC_Geometry::glDraw
+	 */
+	virtual void glExecute(bool, bool forceWire= false);
+	
+	
+protected:
+	//! Virtual interface for OpenGL Geometry set up.
+	/*! This Virtual function have to be implemented in concrete class.*/
+	virtual void glDraw(void) = 0;
+
+	//! Virtual interface for OpenGL Geometry properties.
+	/*! This Virtual function can be modify in concrete class.
+	 * bool IsSelected
+	 * bool ForceWire
+	 */
+	virtual void glPropGeom(bool, bool);
+	
+	//! Vbo creation
+	void createVBOs();
+//@}
+//////////////////////////////////////////////////////////////////////
+// Protected members
+//////////////////////////////////////////////////////////////////////
+protected:
+
+	//! VBO Vertex Buffer
+	GLuint m_VertexBufferID;
+
+	//! VBO Normal Buffer
+	GLuint m_NormalBufferID;
+
+	//! VBO Index Buffer
+	GLuint m_IndexBufferID;
+
+	//! VBO Color Buffer
+	GLuint m_ColorBufferID;
+
+	//! Geometry validity
+	bool m_GeometryIsValid;
+
+	//! Material
+	GLC_Material* m_pMaterial;
+	
+	//! Color per vertex usage
+	bool m_UseColorPerVertex;
+	
+//////////////////////////////////////////////////////////////////////
+// Private members 
+//////////////////////////////////////////////////////////////////////
+private:		
+	//! Geometry type is wire
+	bool m_IsWire;
+
+	//! Transparency
+	bool m_IsTransparent;
+};
+
+#endif /*GLC_VBOGEOM_H_*/
