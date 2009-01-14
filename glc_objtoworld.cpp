2398c2933eb13c2e35a85a1dd3af2d94f9f2a330
laumaya <laumaya@3a52cc46-332c-0410-a947-8d4cafa376f5>
2009-01-14 22:12:37 +0000

    if a mesh as no specific material, rename default material with mesh name.

--- glc_objtoworld.cpp
+++ glc_objtoworld.cpp
@@ -340,6 +340,7 @@ void GLC_ObjToWorld::changeGroup(QString line)
 			}
 			m_pCurrentMesh= new GLC_Mesh2();
 			m_pCurrentMesh->setName(groupName);
+			m_pCurrentMesh->material()->setName(QString("Mat_") + groupName);
 
 		}
 	}
