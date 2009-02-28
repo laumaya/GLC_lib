b06f9c5937ffefce1763c7d667346a075607bc9f
laumaya <laumaya@3a52cc46-332c-0410-a947-8d4cafa376f5>
2009-02-28 20:23:52 +0000

    Update Version in header.

--- glc_offtoworld.cpp
+++ glc_offtoworld.cpp
@@ -3,7 +3,7 @@
 
  This file is part of the GLC-lib library.
  Copyright (C) 2005-2008 Laurent Ribon (laumaya@users.sourceforge.net)
- Version 1.0.0, packaged on August, 2008.
+ Version 1.1.0, packaged on March, 2009.
 
  http://glc-lib.sourceforge.net
 
