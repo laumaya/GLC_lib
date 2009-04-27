0e7f4dbff6b588d966ac8f48c258369d3c8604ff
laumaya <laumaya@3a52cc46-332c-0410-a947-8d4cafa376f5>
2009-04-27 19:13:53 +0000

    Use qFuzzyCompare function instead of comparaison with glc::EPSILON to determine if to double or float are equal.

--- maths/glc_vector3d.h
+++ maths/glc_vector3d.h
@@ -179,8 +179,8 @@ public:
 	//! Return true if the vector is null
 	inline bool isNull(void) const
 	{
-		return (fabs(vector[0]) < glc::EPSILON) && (fabs(vector[1]) < glc::EPSILON)
-		&& (fabs(vector[2]) < glc::EPSILON);
+		return qFuzzyCompare(vector[0], 0.0) and qFuzzyCompare(vector[1], 0.0)
+		and qFuzzyCompare(vector[2], 0.0);
 	}
 
 //@}
