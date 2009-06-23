42dbfb5cc4525ff13f3f3a1fcf627027c56c20f4
laumaya <laumaya@3a52cc46-332c-0410-a947-8d4cafa376f5>
2009-06-23 20:16:08 +0000

    Refactoring : renaming GLC_Instance to GLC_3DViewInstance.

--- sceneGraph/glc_structreference.cpp
+++ sceneGraph/glc_structreference.cpp
@@ -39,9 +39,9 @@ GLC_StructReference::GLC_StructReference(const QString& name)
 }
 
 // Create reference with representation
-GLC_StructReference::GLC_StructReference(const GLC_Instance& rep)
+GLC_StructReference::GLC_StructReference(const GLC_3DViewInstance& rep)
 : m_ListOfInstance()
-, m_pRepresentation(new GLC_Instance(rep))
+, m_pRepresentation(new GLC_3DViewInstance(rep))
 , m_Name(m_pRepresentation->name())
 , m_pAttributes(NULL)
 {
@@ -70,10 +70,10 @@ GLC_StructInstance* GLC_StructReference::createStructInstance()
 // Set Functions
 //////////////////////////////////////////////////////////////////////
 // Set the reference representation
-void GLC_StructReference::setRepresentation(const GLC_Instance& rep)
+void GLC_StructReference::setRepresentation(const GLC_3DViewInstance& rep)
 {
 	Q_ASSERT(NULL == m_pRepresentation);
-	m_pRepresentation= new GLC_Instance(rep);
+	m_pRepresentation= new GLC_3DViewInstance(rep);
 
 	// Update all occurence of this reference if exist
 	if (haveStructInstance())
