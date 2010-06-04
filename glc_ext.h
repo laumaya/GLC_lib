4578f01b9e7d70d0b128537909d401919e9d20f7
laumaya <laumaya@3a52cc46-332c-0410-a947-8d4cafa376f5>
2010-06-04 17:11:58 +0000

    Change licence to LGPL V3

--- glc_ext.h
+++ glc_ext.h
@@ -7,16 +7,16 @@
  http://glc-lib.sourceforge.net
 
  GLC-lib is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
  (at your option) any later version.
 
  GLC-lib is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+ GNU Lesser General Public License for more details.
 
- You should have received a copy of the GNU General Public License
+ You should have received a copy of the GNU Lesser General Public License
  along with GLC-lib; if not, write to the Free Software
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
@@ -27,7 +27,7 @@
 #define GLC_EXT_H_
 
 #include <QtOpenGL>
-#include "glext/glext.h"
+#include "3rdparty/glext/glext.h"
 
 // Buffer offset used by VBO
 #define BUFFER_OFFSET(i) ((char*)NULL + (i))
--- glc_factory.cpp
+++ glc_factory.cpp
@@ -7,16 +7,16 @@
  http://glc-lib.sourceforge.net
 
  GLC-lib is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
  (at your option) any later version.
 
  GLC-lib is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+ GNU Lesser General Public License for more details.
 
- You should have received a copy of the GNU General Public License
+ You should have received a copy of the GNU Lesser General Public License
  along with GLC-lib; if not, write to the Free Software
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
@@ -145,6 +145,12 @@ GLC_3DRep GLC_Factory::createCylinder(double radius, double length) const
 	return newCylinder;
 }
 
+GLC_3DRep GLC_Factory::createCone(double radius, double length) const
+{
+	GLC_3DRep newCone(new GLC_Cone(radius, length));
+	return newCone;
+
+}
 GLC_3DRep GLC_Factory::createRectangle(double l1, double l2)
 {
 	GLC_3DRep newRectangle(new GLC_Rectangle(l1, l2));
