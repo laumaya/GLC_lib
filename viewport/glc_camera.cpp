e0435821786629ca47c9afea6f2cbd7198c08b11
laumaya <laumaya@3a52cc46-332c-0410-a947-8d4cafa376f5>
2011-09-01 19:48:52 +0000

    Use matrix instead of gluLookAt

--- viewport/glc_camera.cpp
+++ viewport/glc_camera.cpp
@@ -431,9 +431,7 @@ GLC_Camera GLC_Camera::isoView() const
 //////////////////////////////////////////////////////////////////////
 void GLC_Camera::glExecute()
 {
-	gluLookAt(m_Eye.x(), m_Eye.y(), m_Eye.z(),
-		m_Target.x(), m_Target.y(), m_Target.z(),
-		m_VectUp.x(), m_VectUp.y(), m_VectUp.z());
+	glMultMatrixd(modelViewMatrix().getData());
 }
 
 //////////////////////////////////////////////////////////////////////
