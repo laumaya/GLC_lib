b4563703881e64c8cb7ef8515fd552daa8e8c1b8
laumaya <laumaya@3a52cc46-332c-0410-a947-8d4cafa376f5>
2011-11-01 10:06:51 +0000

    Add the possibility to get 3DRep volume.

--- geometry/glc_3drep.h
+++ geometry/glc_3drep.h
@@ -121,6 +121,9 @@ public:
 	//! Return materials Set of this 3DRep
 	QSet<GLC_Material*> materialSet() const;
 
+	//! Return the volume of this 3DRep
+	double volume() const;
+
 //@}
 
 //////////////////////////////////////////////////////////////////////
