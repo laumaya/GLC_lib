834d58b3ab73f332510fc99073ba12873294c956
Laurent Ribon <laurent.ribon@gmail.com>
2013-06-17 17:47:25 +0200

    add some assertions

--- src/glc_state.cpp
+++ src/glc_state.cpp
@@ -22,6 +22,8 @@
 
 //! \file glc_state.cpp implementation of the GLC_State class.
 
+#include <QtDebug>
+
 #include "glc_state.h"
 #include "glc_ext.h"
 #include "sceneGraph/glc_octree.h"
@@ -57,141 +59,160 @@ GLC_State::~GLC_State()
 
 bool GLC_State::vboSupported()
 {
-	return m_VboSupported;
+    Q_ASSERT(m_IsValid);
+    return m_VboSupported;
 }
 
 bool GLC_State::vboUsed()
 {
-	return m_UseVbo;
+    Q_ASSERT(m_IsValid);
+    return m_UseVbo;
 }
 
 bool GLC_State::glslSupported()
 {
-	return m_GlslSupported;
+    Q_ASSERT(m_IsValid);
+    return m_GlslSupported;
 }
 
 bool GLC_State::frameBufferSupported()
 {
+    Q_ASSERT(m_IsValid);
     return m_IsFrameBufferSupported;
 }
 
 bool GLC_State::frameBufferBlitSupported()
 {
+    Q_ASSERT(m_IsValid);
     return m_IsFrameBufferBlitSupported;
 }
 
 bool GLC_State::glslUsed()
 {
-	return m_UseShader && m_GlslSupported;
+    Q_ASSERT(m_IsValid);
+    return m_UseShader && m_GlslSupported;
 }
 
 bool GLC_State::pointSpriteSupported()
 {
-	return m_PointSpriteSupported;
+    Q_ASSERT(m_IsValid);
+    return m_PointSpriteSupported;
 }
 
 bool GLC_State::selectionShaderUsed()
 {
-	return m_UseSelectionShader;
+    Q_ASSERT(m_IsValid);
+    return m_UseSelectionShader;
 }
 
 bool GLC_State::isInSelectionMode()
 {
-	return m_IsInSelectionMode;
+    Q_ASSERT(m_IsValid);
+    return m_IsInSelectionMode;
 }
 
 QString GLC_State::version()
 {
-	return m_Version;
+    Q_ASSERT(m_IsValid);
+    return m_Version;
 }
 
 QString GLC_State::vendor()
 {
-	return m_Vendor;
+    Q_ASSERT(m_IsValid);
+    return m_Vendor;
 }
 
 QString GLC_State::renderer()
 {
-	return m_Renderer;
+    Q_ASSERT(m_IsValid);
+    return m_Renderer;
 }
 
 bool GLC_State::vendorIsNvidia()
 {
-	return m_Vendor.contains("NVIDIA");
+    Q_ASSERT(m_IsValid);
+    return m_Vendor.contains("NVIDIA");
 }
 
 bool GLC_State::isPixelCullingActivated()
 {
-	return m_IsPixelCullingActivated;
+    Q_ASSERT(m_IsValid);
+    return m_IsPixelCullingActivated;
 }
 
 bool GLC_State::cacheIsUsed()
 {
-	return m_UseCache;
+    Q_ASSERT(m_IsValid);
+    return m_UseCache;
 }
 
 GLC_CacheManager& GLC_State::currentCacheManager()
 {
-	return m_CacheManager;
+    Q_ASSERT(m_IsValid);
+    return m_CacheManager;
 }
 
 bool GLC_State::isSpacePartitionningActivated()
 {
-	return m_IsSpacePartitionningActivated;
+    Q_ASSERT(m_IsValid);
+    return m_IsSpacePartitionningActivated;
 }
 
 int GLC_State::defaultOctreeDepth()
 {
-	return GLC_Octree::defaultDepth();
+    Q_ASSERT(m_IsValid);
+    return GLC_Octree::defaultDepth();
 }
 
 bool GLC_State::isFrustumCullingActivated()
 {
-	return m_IsFrustumCullingActivated;
+    Q_ASSERT(m_IsValid);
+    return m_IsFrustumCullingActivated;
 }
 
 void GLC_State::init()
 {
-	if (!m_IsValid)
-	{
-		Q_ASSERT((NULL != QGLContext::currentContext()) &&  QGLContext::currentContext()->isValid());
-		setVboSupport();
-		setGlslSupport();
-		setPointSpriteSupport();
-		setFrameBufferSupport();
-		m_Version= (char *) glGetString(GL_VERSION);
-		m_Vendor= (char *) glGetString(GL_VENDOR);
-		m_Renderer= (char *) glGetString(GL_RENDERER);
+    if (!m_IsValid)
+    {
+        Q_ASSERT((NULL != QGLContext::currentContext()) &&  QGLContext::currentContext()->isValid());
+        setVboSupport();
+        setGlslSupport();
+        setPointSpriteSupport();
+        setFrameBufferSupport();
+        m_Version= (char *) glGetString(GL_VERSION);
+        m_Vendor= (char *) glGetString(GL_VENDOR);
+        m_Renderer= (char *) glGetString(GL_RENDERER);
 
-		m_IsValid= true;
-	}
+        m_IsValid= true;
+    }
 }
 
 bool GLC_State::isValid()
 {
-	return m_IsValid;
+    return m_IsValid;
 }
 
 void GLC_State::setVboSupport()
 {
-	m_VboSupported= glc::extensionIsSupported("ARB_vertex_buffer_object") && glc::loadVboExtension();
-	setVboUsage(m_UseVbo);
+    m_VboSupported= glc::extensionIsSupported("ARB_vertex_buffer_object") && glc::loadVboExtension();
+    setVboUsage(m_UseVbo);
 }
 
 void GLC_State::setVboUsage(const bool vboUsed)
 {
-	m_UseVbo= m_VboSupported && vboUsed;
+    m_UseVbo= m_VboSupported && vboUsed;
 }
 
 void GLC_State::setGlslSupport()
 {
-	m_GlslSupported= glc::extensionIsSupported("GL_ARB_shading_language_100") && glc::loadGlSlExtension();
-	setGlslUsage(m_UseShader);
+    m_GlslSupported= glc::extensionIsSupported("GL_ARB_shading_language_100") && glc::loadGlSlExtension();
+    setGlslUsage(m_UseShader);
 }
 
 void GLC_State::setPointSpriteSupport()
 {
-	m_PointSpriteSupported= glc::extensionIsSupported("GL_ARB_point_parameters") && glc::loadPointSpriteExtension();
+    m_PointSpriteSupported= glc::extensionIsSupported("GL_ARB_point_parameters") && glc::loadPointSpriteExtension();
 }
 
 void GLC_State::setFrameBufferSupport()
@@ -206,45 +227,45 @@ void GLC_State::setFrameBufferBlitSupport()
 
 void GLC_State::setGlslUsage(const bool glslUsage)
 {
-	m_UseShader= m_GlslSupported && glslUsage;
+    m_UseShader= m_GlslSupported && glslUsage;
 }
 
 void GLC_State::setSelectionShaderUsage(const bool shaderUsed)
 {
-	m_UseSelectionShader= shaderUsed && m_GlslSupported;
+    m_UseSelectionShader= shaderUsed && m_GlslSupported;
 }
 
 void GLC_State::setSelectionMode(const bool mode)
 {
-	m_IsInSelectionMode= mode;
+    m_IsInSelectionMode= mode;
 }
 
 void GLC_State::setPixelCullingUsage(const bool activation)
 {
-	m_IsPixelCullingActivated= activation;
+    m_IsPixelCullingActivated= activation;
 }
 
 void GLC_State::setCacheUsage(const bool cacheUsage)
 {
-	m_UseCache= cacheUsage;
+    m_UseCache= cacheUsage;
 }
 
 void GLC_State::setCurrentCacheManager(const GLC_CacheManager& cacheManager)
 {
-	m_CacheManager= cacheManager;
+    m_CacheManager= cacheManager;
 }
 
 void GLC_State::setSpacePartionningUsage(const bool usage)
 {
-	m_IsSpacePartitionningActivated= usage;
+    m_IsSpacePartitionningActivated= usage;
 }
 
 void GLC_State::setDefaultOctreeDepth(int depth)
 {
-	GLC_Octree::setDefaultDepth(depth);
+    GLC_Octree::setDefaultDepth(depth);
 }
 
 void GLC_State::setFrustumCullingUsage(bool usage)
 {
-	m_IsFrustumCullingActivated= usage;
+    m_IsFrustumCullingActivated= usage;
 }
