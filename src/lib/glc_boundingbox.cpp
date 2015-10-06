6abec1a44acb3d131eeee6755313bff3008f3055
Laurent Ribon <laurent.ribon@gmail.com>
2015-10-06 11:20:49 +0200

    Fix intersection computation issue Add virtual destructor

--- src/lib/glc_boundingbox.cpp
+++ src/lib/glc_boundingbox.cpp
@@ -54,41 +54,46 @@ GLC_BoundingBox::GLC_BoundingBox(const GLC_Point3d& lower, const GLC_Point3d& up
 
 }
 
+GLC_BoundingBox::~GLC_BoundingBox()
+{
+
+}
+
 //////////////////////////////////////////////////////////////////////
 // Get Functions
 //////////////////////////////////////////////////////////////////////
 
 quint32 GLC_BoundingBox::chunckID()
 {
-	return m_ChunkId;
+    return m_ChunkId;
 }
 
 bool GLC_BoundingBox::intersect(const GLC_Point3d& point) const
 {
-	if (!m_IsEmpty)
-	{
-		bool result= (point.x() < m_Upper.x()) && (point.y() < m_Upper.y())
-		&& (point.z() < m_Upper.z()) && (point.x() > m_Lower.x())
-		&& (point.y() > m_Lower.y()) && (point.z() > m_Lower.z());
+    if (!m_IsEmpty)
+    {
+        bool result= (point.x() < m_Upper.x()) && (point.y() < m_Upper.y())
+        && (point.z() < m_Upper.z()) && (point.x() > m_Lower.x())
+        && (point.y() > m_Lower.y()) && (point.z() > m_Lower.z());
 
-		return result;
-	}
-	else
-	{
-		return false;
-	}
+        return result;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 bool GLC_BoundingBox::intersectBoundingSphere(const GLC_Point3d& point) const
 {
-	const double distance= (center() - point).length();
-	return distance < boundingSphereRadius();
+    const double distance= (center() - point).length();
+    return distance < boundingSphereRadius();
 }
 
 bool GLC_BoundingBox::intersectBoundingSphere(const GLC_BoundingBox& boundingSphere) const
 {
-	const double distance= (center() - boundingSphere.center()).length();
-	return distance < (boundingSphereRadius() + boundingSphere.boundingSphereRadius());
+    const double distance= (center() - boundingSphere.center()).length();
+    return distance < (boundingSphereRadius() + boundingSphere.boundingSphereRadius());
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -97,138 +102,138 @@ bool GLC_BoundingBox::intersectBoundingSphere(const GLC_BoundingBox& boundingSph
 
 GLC_BoundingBox& GLC_BoundingBox::combine(const GLC_Point3d& point)
 {
-	if (m_IsEmpty)
-	{
-		m_Lower= point;
-		m_Upper= point;
-		m_IsEmpty= false;
-	}
-	else
-	{
-		double lowerX= qMin(point.x(), m_Lower.x());
-		double lowerY= qMin(point.y(), m_Lower.y());
-		double lowerZ= qMin(point.z(), m_Lower.z());
-		m_Lower.setVect(lowerX, lowerY, lowerZ);
+    if (m_IsEmpty)
+    {
+        m_Lower= point;
+        m_Upper= point;
+        m_IsEmpty= false;
+    }
+    else
+    {
+        double lowerX= qMin(point.x(), m_Lower.x());
+        double lowerY= qMin(point.y(), m_Lower.y());
+        double lowerZ= qMin(point.z(), m_Lower.z());
+        m_Lower.setVect(lowerX, lowerY, lowerZ);
 
-		double upperX= qMax(point.x(), m_Upper.x());
-		double upperY= qMax(point.y(), m_Upper.y());
-		double upperZ= qMax(point.z(), m_Upper.z());
-		m_Upper.setVect(upperX, upperY, upperZ);
-	}
-	return *this;
+        double upperX= qMax(point.x(), m_Upper.x());
+        double upperY= qMax(point.y(), m_Upper.y());
+        double upperZ= qMax(point.z(), m_Upper.z());
+        m_Upper.setVect(upperX, upperY, upperZ);
+    }
+    return *this;
 }
 
 GLC_BoundingBox& GLC_BoundingBox::combine(const GLC_Point3df& pointf)
 {
-	GLC_Point3d point(pointf);
-	if (m_IsEmpty)
-	{
-		m_Lower= point;
-		m_Upper= point;
-		m_IsEmpty= false;
-	}
-	else
-	{
-		double lowerX= qMin(point.x(), m_Lower.x());
-		double lowerY= qMin(point.y(), m_Lower.y());
-		double lowerZ= qMin(point.z(), m_Lower.z());
-		m_Lower.setVect(lowerX, lowerY, lowerZ);
+    GLC_Point3d point(pointf);
+    if (m_IsEmpty)
+    {
+        m_Lower= point;
+        m_Upper= point;
+        m_IsEmpty= false;
+    }
+    else
+    {
+        double lowerX= qMin(point.x(), m_Lower.x());
+        double lowerY= qMin(point.y(), m_Lower.y());
+        double lowerZ= qMin(point.z(), m_Lower.z());
+        m_Lower.setVect(lowerX, lowerY, lowerZ);
 
-		double upperX= qMax(point.x(), m_Upper.x());
-		double upperY= qMax(point.y(), m_Upper.y());
-		double upperZ= qMax(point.z(), m_Upper.z());
-		m_Upper.setVect(upperX, upperY, upperZ);
-	}
-	return *this;
+        double upperX= qMax(point.x(), m_Upper.x());
+        double upperY= qMax(point.y(), m_Upper.y());
+        double upperZ= qMax(point.z(), m_Upper.z());
+        m_Upper.setVect(upperX, upperY, upperZ);
+    }
+    return *this;
 }
 
 GLC_BoundingBox& GLC_BoundingBox::combine(const GLC_BoundingBox& box)
 {
-	if (m_IsEmpty && !box.m_IsEmpty)
-	{
-		m_Lower= box.m_Lower;
-		m_Upper= box.m_Upper;
-		m_IsEmpty= box.m_IsEmpty;
-	}
-	else if (! box.m_IsEmpty)
-	{
-		double lowerX= qMin(box.m_Lower.x(), m_Lower.x());
-		double lowerY= qMin(box.m_Lower.y(), m_Lower.y());
-		double lowerZ= qMin(box.m_Lower.z(), m_Lower.z());
-		m_Lower.setVect(lowerX, lowerY, lowerZ);
+    if (m_IsEmpty && !box.m_IsEmpty)
+    {
+        m_Lower= box.m_Lower;
+        m_Upper= box.m_Upper;
+        m_IsEmpty= box.m_IsEmpty;
+    }
+    else if (! box.m_IsEmpty)
+    {
+        double lowerX= qMin(box.m_Lower.x(), m_Lower.x());
+        double lowerY= qMin(box.m_Lower.y(), m_Lower.y());
+        double lowerZ= qMin(box.m_Lower.z(), m_Lower.z());
+        m_Lower.setVect(lowerX, lowerY, lowerZ);
 
-		double upperX= qMax(box.m_Upper.x(), m_Upper.x());
-		double upperY= qMax(box.m_Upper.y(), m_Upper.y());
-		double upperZ= qMax(box.m_Upper.z(), m_Upper.z());
-		m_Upper.setVect(upperX, upperY, upperZ);
-	}
+        double upperX= qMax(box.m_Upper.x(), m_Upper.x());
+        double upperY= qMax(box.m_Upper.y(), m_Upper.y());
+        double upperZ= qMax(box.m_Upper.z(), m_Upper.z());
+        m_Upper.setVect(upperX, upperY, upperZ);
+    }
 
-	return *this;
+    return *this;
 }
 
 GLC_BoundingBox& GLC_BoundingBox::transform(const GLC_Matrix4x4& matrix)
 {
-	if (!m_IsEmpty)
-	{
-		// Compute Transformed BoundingBox Corner
-		GLC_Point3d corner1(m_Lower);
-		GLC_Point3d corner7(m_Upper);
-		GLC_Point3d corner2(corner7.x(), corner1.y(), corner1.z());
-		GLC_Point3d corner3(corner7.x(), corner7.y(), corner1.z());
-		GLC_Point3d corner4(corner1.x(), corner7.y(), corner1.z());
-		GLC_Point3d corner5(corner1.x(), corner1.y(), corner7.z());
-		GLC_Point3d corner6(corner7.x(), corner1.y(), corner7.z());
-		GLC_Point3d corner8(corner1.x(), corner7.y(), corner7.z());
+    if (!m_IsEmpty)
+    {
+        // Compute Transformed BoundingBox Corner
+        GLC_Point3d corner1(m_Lower);
+        GLC_Point3d corner7(m_Upper);
+        GLC_Point3d corner2(corner7.x(), corner1.y(), corner1.z());
+        GLC_Point3d corner3(corner7.x(), corner7.y(), corner1.z());
+        GLC_Point3d corner4(corner1.x(), corner7.y(), corner1.z());
+        GLC_Point3d corner5(corner1.x(), corner1.y(), corner7.z());
+        GLC_Point3d corner6(corner7.x(), corner1.y(), corner7.z());
+        GLC_Point3d corner8(corner1.x(), corner7.y(), corner7.z());
 
-		corner1 = (matrix * corner1);
-		corner2 = (matrix * corner2);
-		corner3 = (matrix * corner3);
-		corner4 = (matrix * corner4);
-		corner5 = (matrix * corner5);
-		corner6 = (matrix * corner6);
-		corner7 = (matrix * corner7);
-		corner8 = (matrix * corner8);
+        corner1 = (matrix * corner1);
+        corner2 = (matrix * corner2);
+        corner3 = (matrix * corner3);
+        corner4 = (matrix * corner4);
+        corner5 = (matrix * corner5);
+        corner6 = (matrix * corner6);
+        corner7 = (matrix * corner7);
+        corner8 = (matrix * corner8);
 
-		// Compute the new BoundingBox
-		GLC_BoundingBox boundingBox;
-		boundingBox.combine(corner1);
-		boundingBox.combine(corner2);
-		boundingBox.combine(corner3);
-		boundingBox.combine(corner4);
-		boundingBox.combine(corner5);
-		boundingBox.combine(corner6);
-		boundingBox.combine(corner7);
-		boundingBox.combine(corner8);
+        // Compute the new BoundingBox
+        GLC_BoundingBox boundingBox;
+        boundingBox.combine(corner1);
+        boundingBox.combine(corner2);
+        boundingBox.combine(corner3);
+        boundingBox.combine(corner4);
+        boundingBox.combine(corner5);
+        boundingBox.combine(corner6);
+        boundingBox.combine(corner7);
+        boundingBox.combine(corner8);
 
-		m_Lower= boundingBox.m_Lower;
-		m_Upper= boundingBox.m_Upper;
-	}
+        m_Lower= boundingBox.m_Lower;
+        m_Upper= boundingBox.m_Upper;
+    }
 
-	return *this;
+    return *this;
 }
 
 QDataStream &operator<<(QDataStream &stream, const GLC_BoundingBox &bBox)
 {
-	quint32 chunckId= GLC_BoundingBox::m_ChunkId;
-	stream << chunckId;
+    quint32 chunckId= GLC_BoundingBox::m_ChunkId;
+    stream << chunckId;
 
-	stream << bBox.m_IsEmpty;
-	stream << bBox.m_Lower;
-	stream << bBox.m_Upper;
+    stream << bBox.m_IsEmpty;
+    stream << bBox.m_Lower;
+    stream << bBox.m_Upper;
 
-	return stream;
+    return stream;
 }
 
 QDataStream &operator>>(QDataStream &stream, GLC_BoundingBox &bBox)
 {
-	quint32 chunckId;
-	stream >> chunckId;
-	Q_ASSERT(chunckId == GLC_BoundingBox::m_ChunkId);
+    quint32 chunckId;
+    stream >> chunckId;
+    Q_ASSERT(chunckId == GLC_BoundingBox::m_ChunkId);
 
-	stream >> bBox.m_IsEmpty;
-	stream >> bBox.m_Lower;
-	stream >> bBox.m_Upper;
+    stream >> bBox.m_IsEmpty;
+    stream >> bBox.m_Lower;
+    stream >> bBox.m_Upper;
 
-	return stream;
+    return stream;
 }
 
--- src/lib/glc_boundingbox.h
+++ src/lib/glc_boundingbox.h
@@ -42,22 +42,24 @@
 
 class GLC_LIB_EXPORT GLC_BoundingBox
 {
-	friend GLC_LIB_EXPORT QDataStream &operator<<(QDataStream &, const GLC_BoundingBox &);
-	friend GLC_LIB_EXPORT QDataStream &operator>>(QDataStream &, GLC_BoundingBox &);
+    friend GLC_LIB_EXPORT QDataStream &operator<<(QDataStream &, const GLC_BoundingBox &);
+    friend GLC_LIB_EXPORT QDataStream &operator>>(QDataStream &, GLC_BoundingBox &);
 
 //////////////////////////////////////////////////////////////////////
-/*! @name Constructor */
+/*! @name Constructor / Destructor */
 //@{
 //////////////////////////////////////////////////////////////////////
 public:
-	//! Construct an empty bounding box
-	GLC_BoundingBox();
+    //! Construct an empty bounding box
+    GLC_BoundingBox();
 
-	//! Construct a bounding box from  the given bounding box
-	GLC_BoundingBox(const GLC_BoundingBox& boundingBox);
+    //! Construct a bounding box from  the given bounding box
+    GLC_BoundingBox(const GLC_BoundingBox& boundingBox);
 
-	//! Construct a bounding box from the given 3d point
-	GLC_BoundingBox(const GLC_Point3d& lower, const GLC_Point3d& upper);
+    //! Construct a bounding box from the given 3d point
+    GLC_BoundingBox(const GLC_Point3d& lower, const GLC_Point3d& upper);
+
+    virtual ~GLC_BoundingBox();
 
 //@}
 //////////////////////////////////////////////////////////////////////
@@ -65,60 +67,60 @@ public:
 //@{
 //////////////////////////////////////////////////////////////////////
 public:
-	//! Return this class Chunk ID
-	static quint32 chunckID();
+    //! Return this class Chunk ID
+    static quint32 chunckID();
 
-	//! Return true if this bounding box is empty
-	bool isEmpty(void) const
-	{
-		return m_IsEmpty;
-	}
+    //! Return true if this bounding box is empty
+    bool isEmpty(void) const
+    {
+        return m_IsEmpty;
+    }
 
-	//! Return true if the given 3d point intersect this bounding box
-	bool intersect(const GLC_Point3d& point) const;
+    //! Return true if the given 3d point intersect this bounding box
+    bool intersect(const GLC_Point3d& point) const;
 
-	//! Return true if the given bounding box intersect this bounding box
-	inline bool intersect(const GLC_BoundingBox& boundingBox) const;
+    //! Return true if the given bounding box intersect this bounding box
+    inline bool intersect(const GLC_BoundingBox& boundingBox) const;
 
-	//! Return true if the given 3d point intersect this bounding sphere of bounding box
-	bool intersectBoundingSphere(const GLC_Point3d&) const;
+    //! Return true if the given 3d point intersect this bounding sphere of bounding box
+    bool intersectBoundingSphere(const GLC_Point3d&) const;
 
-	//! Return true if the given bounding sphere of bounding box intersect the bounding sphere of box bounding box
-	bool intersectBoundingSphere(const GLC_BoundingBox&) const;
+    //! Return true if the given bounding sphere of bounding box intersect the bounding sphere of box bounding box
+    bool intersectBoundingSphere(const GLC_BoundingBox&) const;
 
-	//! Return the lower corner of this bounding box
-	inline const GLC_Point3d& lowerCorner() const
-	{return m_Lower;}
+    //! Return the lower corner of this bounding box
+    inline const GLC_Point3d& lowerCorner() const
+    {return m_Lower;}
 
-	//! Return the upper corner of this bounding box
-	inline const GLC_Point3d& upperCorner() const
-	{return m_Upper;}
+    //! Return the upper corner of this bounding box
+    inline const GLC_Point3d& upperCorner() const
+    {return m_Upper;}
 
-	//! Return the center of this bounding box
-	inline GLC_Point3d center() const;
+    //! Return the center of this bounding box
+    inline GLC_Point3d center() const;
 
-	//! Return the radius of this bounding sphere of bounding box
-	inline double boundingSphereRadius() const
-	{return GLC_Vector3d(m_Lower - m_Upper).length() / 2.0;}
+    //! Return the radius of this bounding sphere of bounding box
+    inline double boundingSphereRadius() const
+    {return GLC_Vector3d(m_Lower - m_Upper).length() / 2.0;}
 
-	//! Return true if this bounding box is equal of the given bounding box
-	inline bool operator == (const GLC_BoundingBox& boundingBox);
+    //! Return true if this bounding box is equal of the given bounding box
+    inline bool operator == (const GLC_BoundingBox& other) const;
 
-	//! Return true if this bounding box is not equal of the given bounding box
-	inline bool operator != (const GLC_BoundingBox& boundingBox)
-	{return !(*this == boundingBox);}
+    //! Return true if this bounding box is not equal of the given bounding box
+    inline bool operator != (const GLC_BoundingBox& boundingBox)
+    {return !(*this == boundingBox);}
 
-	//! Return the length off this bounding box on x axis
-	inline double xLength() const
-	{return fabs(m_Upper.x() - m_Lower.x());}
+    //! Return the length off this bounding box on x axis
+    inline double xLength() const
+    {return fabs(m_Upper.x() - m_Lower.x());}
 
-	//! Return the length off this bounding box on y axis
-	inline double yLength() const
-	{return fabs(m_Upper.y() - m_Lower.y());}
+    //! Return the length off this bounding box on y axis
+    inline double yLength() const
+    {return fabs(m_Upper.y() - m_Lower.y());}
 
-	//! Return the length off this bounding box on z axis
-	inline double zLength() const
-	{return fabs(m_Upper.z() - m_Lower.z());}
+    //! Return the length off this bounding box on z axis
+    inline double zLength() const
+    {return fabs(m_Upper.z() - m_Lower.z());}
 
 
 //@}
@@ -128,17 +130,17 @@ public:
 //@{
 //////////////////////////////////////////////////////////////////////
 public:
-	//! Combine this bounding Box with the given 3d point and return a reference of this bounding box
-	GLC_BoundingBox& combine(const GLC_Point3d& point);
+    //! Combine this bounding Box with the given 3d point and return a reference of this bounding box
+    GLC_BoundingBox& combine(const GLC_Point3d& point);
 
-	//! Combine this bounding Box with the given 3d point and return a reference of this bounding box
-	GLC_BoundingBox& combine(const GLC_Point3df& point);
+    //! Combine this bounding Box with the given 3d point and return a reference of this bounding box
+    GLC_BoundingBox& combine(const GLC_Point3df& point);
 
-	//! Combine this bounding Box with the given bounding box and return a reference of this bounding box
-	GLC_BoundingBox& combine(const GLC_BoundingBox& box);
+    //! Combine this bounding Box with the given bounding box and return a reference of this bounding box
+    GLC_BoundingBox& combine(const GLC_BoundingBox& box);
 
-	//! Transform this bounding Box with the given matrix and return a reference of this bounding box
-	GLC_BoundingBox& transform(const GLC_Matrix4x4& matrix);
+    //! Transform this bounding Box with the given matrix and return a reference of this bounding box
+    GLC_BoundingBox& transform(const GLC_Matrix4x4& matrix);
 
 //@}
 
@@ -146,17 +148,17 @@ public:
 // Private members
 //////////////////////////////////////////////////////////////////////
 private:
-	//! Lower corner 3d point
-	GLC_Point3d m_Lower;
+    //! Lower corner 3d point
+    GLC_Point3d m_Lower;
 
-	//! Upper corner 3d point
-	GLC_Point3d m_Upper;
+    //! Upper corner 3d point
+    GLC_Point3d m_Upper;
 
-	//! Flag to know if this bounding box is empty
-	bool m_IsEmpty;
+    //! Flag to know if this bounding box is empty
+    bool m_IsEmpty;
 
-	//! This class chunk id
-	static quint32 m_ChunkId;
+    //! This class chunk id
+    static quint32 m_ChunkId;
 };
 
 //! Non-member stream operator
@@ -166,28 +168,43 @@ GLC_LIB_EXPORT QDataStream &operator>>(QDataStream &, GLC_BoundingBox &);
 // Return true if the given bounding box intersect this bounding box
 bool GLC_BoundingBox::intersect(const GLC_BoundingBox& boundingBox) const
 {
-	// Distance between bounding box center
-	GLC_Vector3d thisCenter= center();
-	GLC_Vector3d otherCenter= boundingBox.center();
-	const double distanceX= fabs(thisCenter.x() - otherCenter.x());
-	const double distanceY= fabs(thisCenter.y() - otherCenter.y());
-	const double distanceZ= fabs(thisCenter.z() - otherCenter.z());
+    bool subject= false;
+    // Distance between bounding box center
+    const GLC_Point3d thisCenter= center();
+    const GLC_Point3d otherCenter= boundingBox.center();
 
-	bool intersect= distanceX < ((xLength() + boundingBox.xLength()) * 0.5);
-	intersect= intersect && (distanceY < ((yLength() + boundingBox.yLength()) * 0.5));
-	intersect= intersect && (distanceZ < ((zLength() + boundingBox.zLength()) * 0.5));
-	return intersect;
+    if (this->intersect(otherCenter) || (boundingBox.intersect(thisCenter)))
+    {
+        subject= true;
+    }
+    else
+    {
+        const double distanceX= fabs(thisCenter.x() - otherCenter.x());
+        const double distanceY= fabs(thisCenter.y() - otherCenter.y());
+        const double distanceZ= fabs(thisCenter.z() - otherCenter.z());
+
+        subject= distanceX < ((xLength() + boundingBox.xLength()) * 0.5);
+        subject= subject && (distanceY < ((yLength() + boundingBox.yLength()) * 0.5));
+        subject= subject && (distanceZ < ((zLength() + boundingBox.zLength()) * 0.5));
+    }
+    return subject;
 }
 
-bool GLC_BoundingBox::operator == (const GLC_BoundingBox& box)
+bool GLC_BoundingBox::operator == (const GLC_BoundingBox& other) const
 {
-	return (m_Lower == box.m_Lower) && (m_Upper == box.m_Upper);
+    bool subject= true;
+    if (this != &other)
+    {
+        subject= (m_Lower == other.m_Lower) && (m_Upper == other.m_Upper);
+    }
+
+    return subject;
 }
 
 GLC_Point3d GLC_BoundingBox::center(void) const
 {
-	GLC_Vector3d vectResult = (m_Lower + m_Upper) * 0.5;
-	return vectResult;
+    GLC_Vector3d vectResult = (m_Lower + m_Upper) * 0.5;
+    return vectResult;
 }
 
 #endif /*GLC_BOUNDINGBOX_*/
